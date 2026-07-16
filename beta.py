1ce763853ed3bfaee11a6074a0ec88732a7a6790
Akram khan <akramkhan50097@gmail.com>
2026-07-16 12:49:22 +0530

    some modifications to the files of python

--- beta.py
+++ beta.py
@@ -1,1 +1,2 @@
 print("Hi my name is Akram Khan, i am persuing btech from shri ramswaroop memorial university, dewa road barabanki/n my core subjects are machine learning and artificial intelligence./n thank you so much guys")
+print("Hello World")
